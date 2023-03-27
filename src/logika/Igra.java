95e3d1277b8662dc07c6057c4aa4e950c376ea10
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2023-03-27 21:11:21 +0200

    Osnovni skelet po navodilih iz spletne učilnice

---
+++ src/logika/Igra.java
@@ -0,0 +1,18 @@
+package logika;
+
+import splosno.Poteza;
+
+public class Igra {
+
+	public Igra() {
+		// TODO: Konstruktor mora ustvariti novo igro velikosti 9x9
+		// igro začne črni igralec
+	}
+	
+	public boolean odigraj(Poteza poteza) {
+		// TODO: funkcija odigra dano potezo, če je možna
+		// vrne true, če je bila poteza možna, sicer false
+		// Poteza(0,0) je levo zgoraj, Poteza(8,8) pa desno spodaj
+		return false;
+	}
+}
