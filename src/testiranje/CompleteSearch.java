31d92752715d34344bf4104d588dc2b1a713087e
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2023-04-12 09:52:00 +0200

    ideja za ustvaritev funkcije

--- src/testiranje/CompleteSearch.java
+++ src/testiranje/CompleteSearch.java
@@ -12,46 +12,55 @@ public class CompleteSearch {
 	
 	private record DveStevili(int a, int b) {}
 	
-	private static DveStevili search(Mreza mreza, BarvaIgralca igralec, int globina) {
+	private static DveStevili search(Mreza mreza, BarvaIgralca igralec, int globina, boolean doOutput) {
 		int zmage = 0;
 		int zgube = 0;
 		for (Indeks idx : mreza.prostaPolja()) {
 			mreza.postaviBarvo(idx, BarvaPolja.igralcevaBarva(igralec));
-			mreza.izpisi();
+			if (doOutput)
+				mreza.izpisi();
 			if (mreza.jeBarvaIzgubila(BarvaPolja.igralcevaBarva(BarvaIgralca.novaBarva(igralec)))) {
-				System.out.println("Zmaga za " + igralec);
+				if (doOutput)
+					System.out.println("Zmaga za " + igralec);
 				zmage++;
 			} else if (mreza.jeBarvaIzgubila(BarvaPolja.igralcevaBarva(igralec))) {
-				System.out.println("Izguba za " + igralec);
+				if (doOutput)
+					System.out.println("Izguba za " + igralec);
 				zgube++;
 			} else {
-				System.out.format("Branchpoint (depth=%d).\n", globina);
-				DveStevili ns = search(mreza, BarvaIgralca.novaBarva(igralec), globina+1);
+				if (doOutput)
+					System.out.format("Branchpoint (depth=%d).\n", globina);
+				DveStevili ns = search(mreza, BarvaIgralca.novaBarva(igralec), globina+1, doOutput);
 				zmage += ns.b;
 				zgube += ns.a;
-				System.out.format("Branchpoint (depth=%d) backtrack. Zmage/izgube za %s: %d/%d\n", globina, igralec.toString(), ns.b, ns.a);
+				if (doOutput)
+					System.out.format("Branchpoint (depth=%d) backtrack. Zmage/izgube za %s: %d/%d\n", globina, igralec.toString(), ns.b, ns.a);
 			}
 			mreza.postaviBarvo(idx, BarvaPolja.PRAZNA);
 		}
 		return new DveStevili(zmage, zgube);
 	}
 	
-	private static final int N = 4;
+	/*private static final int N = 9;
 	private static final Indeks[] zacetnePoteze = {
 			new Indeks(0, 1), new Indeks(1, 2), new Indeks(1, 1), new Indeks(2, 1),
 			new Indeks(2, 2), new Indeks(2, 3), new Indeks(3, 2)
-	};
+	};*/
 
 	public static void main(String[] args) {
-		Mreza mreza = new Mreza(N, N);
+		/*Mreza mreza = new Mreza(N, N);
 		BarvaIgralca barva = BarvaIgralca.CRNA;
 		for (Indeks idx : zacetnePoteze) {
 			mreza.postaviBarvo(idx, BarvaPolja.igralcevaBarva(barva));
 			barva = BarvaIgralca.novaBarva(barva);
 		}
+		*/
+		Mreza mreza = RandomMrezaGenerator.nakljucnaMreza(9, 0.6);
+		BarvaIgralca barva = BarvaIgralca.CRNA;
 		mreza.izpisi();
 		System.out.println();
-		search(mreza, barva, 0);
+		DveStevili ns = search(mreza, barva, 0, false);
+		System.out.format("Zmage/izgube za %s: %d/%d\n", barva.toString(), ns.a, ns.b);
 	}
 
 }
