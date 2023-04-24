89ca6d74b945ed3ee0d05b3aa77034c392a3b503
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2023-04-24 09:25:49 +0200

    Manjša izboljšava določevalne funkcije

--- src/inteligenca/PrimitivniOcenjevalecMreze.java
+++ src/inteligenca/PrimitivniOcenjevalecMreze.java
@@ -13,6 +13,7 @@ public class PrimitivniOcenjevalecMreze extends OcenjevalecMreze {
 	double oceniMrezo(Mreza mreza, BarvaIgralca igralec) {
 		int mojeTocke = mreza.minSteviloSvobodBarve(igralec.polje());
 		int njegoveTocke = mreza.minSteviloSvobodBarve(igralec.naslednji().polje());
-		return mojeTocke - njegoveTocke;
+		return mojeTocke - njegoveTocke + 2.5 * mreza.povprecnaRazdaljaOdRoba(igralec.polje()) / 9.0
+				- 2.5 * mreza.povprecnaRazdaljaOdRoba(igralec.naslednji().polje()) / 9.0;
 	}
 }
