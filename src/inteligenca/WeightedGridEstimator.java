ccbf778715f3a27ca172eee239c277a46bd4ffa8
Patrik Žnidaršič <posta.patrik.2@gmail.com>
2023-05-03 09:40:56 +0200

    Dodan konstruktor za custom uteži v WeigtedGridEstimator

--- src/inteligenca/WeightedGridEstimator.java
+++ src/inteligenca/WeightedGridEstimator.java
@@ -3,21 +3,41 @@ package inteligenca;
 import logika.Grid;
 import logika.PlayerColor;
 
+@SuppressWarnings("serial")
+class InvalidWeightNumberException extends Exception {}
+
 public class WeightedGridEstimator extends GridEstimator {
 	
-	double myComponentNumberWeight = -0.2;
-	double theirComponentNumberWeight = 0.2;
-	double myWorstLibertyNumberWeight = 1.5;
-	double theirWorstLibertyNumberWeight = -1.5;
-	double myAverageLibertyNumberWeight = 1;
-	double theirAverageLibertyNumberWeight = -1;
-	double myBorderDistanceWeiht = 0.2;
-	double theirBorderDistanceWeight = -0.2;
-	double myMinLibertySizeWeight = 2;
-	double theirMinLibertySizeWeight = -2;
+	private double myComponentNumberWeight = -0.2;
+	private double theirComponentNumberWeight = 0.2;
+	private double myWorstLibertyNumberWeight = 1.5;
+	private double theirWorstLibertyNumberWeight = -1.5;
+	private double myAverageLibertyNumberWeight = 1;
+	private double theirAverageLibertyNumberWeight = -1;
+	private double myBorderDistanceWeiht = 0.2;
+	private double theirBorderDistanceWeight = -0.2;
+	private double myMinLibertySizeWeight = 2;
+	private double theirMinLibertySizeWeight = -2;
 
 	public WeightedGridEstimator() {
 		super("weighted");
+	}
+	
+	public WeightedGridEstimator(double[] weights) throws InvalidWeightNumberException {
+		super("weighted-custom");
+		if (weights.length != 10) 
+			throw new InvalidWeightNumberException();
+		
+		myComponentNumberWeight = weights[0];
+		theirComponentNumberWeight = weights[1];
+		myWorstLibertyNumberWeight = weights[2];
+		theirWorstLibertyNumberWeight = weights[3];
+		myAverageLibertyNumberWeight = weights[4];
+		theirAverageLibertyNumberWeight = weights[5];
+		myBorderDistanceWeiht = weights[6];
+		theirBorderDistanceWeight = weights[7];
+		myMinLibertySizeWeight = weights[8];
+		theirMinLibertySizeWeight = weights[9];
 	}
 
 	@Override
