32e86a3974546488ea32332f90182a0ab77916a3
jakavel <jaka.velkaverh@gmail.com>
2023-05-22 08:47:12 +0200

    Odstranil neuporabljene import-e.

--- src/logika/Grid.java
+++ src/logika/Grid.java
@@ -6,11 +6,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import splosno.Poteza;
--- src/logika/UFDS.java
+++ src/logika/UFDS.java
@@ -1,8 +1,6 @@
 package logika;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
